5c55e1d43c7e2c6930e0443c66bf860af38adfd2
Tim Taylor <timothy.george.taylor@gmail.com>
2020-11-26 10:48:01 -0500

    Connectfour.rb and its spec are now in working condition

--- lib/connectfour.rb
+++ lib/connectfour.rb
@@ -1,10 +1,14 @@
 class Grid
-  WINNING_COMBOS = [['X', 'X', 'X', 'X'],
-                    ['O', 'O', 'O', 'O']]
-
   def create_grid
     Array.new(7) {Array.new(7)}
   end
+
+  def print_grid(board)
+    board.each { |row|
+      p row
+    }
+  end
+
 end
 
 class Gameplay < Grid
@@ -16,24 +20,79 @@ class Gameplay < Grid
     @game_over = false
   end
 
-  def play_piece(player, column, row = 6)
+  def random_generator
+    until @game_over
+      play_piece(rand(6))
+      win_conditions
+    end
+    print_grid(@board)
+    return @game_over
+  end
+
+  def play_piece(column, player = take_turns, row = 6)
     until @board[row][column].nil?
       row -= 1
     end
+    return if row == 0 && @board[row][column] != nil
     @board[row][column] = player
+    @turn_number += 1
     return @board
   end
 
   def take_turns
     player = @turn_number.even? ? 'X' : 'O'
-    @turn_number += 1
     return player
   end
 
-  def win_conditions(board)
-    if WINNING_COMBOS.any? do |combo|
-      combo.all? { |position| @board[position] == 'X' }
+  def win_conditions(board = @board, player = take_turns)
+    tie = @turn_number >= 49 ? true : false
+    h_win = horizontal_check(board, player)
+    v_win = vertical_check(board, player)
+    d_win = diagonal_check(board, player)
+    @game_over = true if h_win || v_win || d_win
+    return @game_over
+  end
+
+  def diagonal_check(board, player)
+    diagonal = []
+    i = 0
+    while i <= 7
+      array = []
+      board.each_with_index { |row, ix|
+        array.push(row[i-ix])
+      }
+      diagonal.push(array)
+      i += 1
     end
-    game_over = true
+    return horizontal_check(diagonal, player)
+  end
+
+  def vertical_check(board, player)
+    vertical = []
+    i = 0
+    while i <= 7 
+      array = []
+      board.each { |row|
+        array.push(row[i])
+      }
+      vertical.push(array)
+      i += 1
+    end
+    return horizontal_check(vertical, player)
+  end
+
+  def horizontal_check(board, player)
+    board.any? { |row|
+      case 
+      when row[0..3].all? { |e| e == player }
+        true
+      when row[1..4].all? { |e| e == player }
+        true
+      when row[2..5].all? { |e| e == player }
+        true
+      when row[3..6].all? { |e| e == player }
+        true
+      end
+    }
   end
 end
--- spec/connectfour_spec.rb
+++ spec/connectfour_spec.rb
@@ -13,8 +13,8 @@ describe Gameplay do
   describe "#play_piece" do
     it "assigns a piece to the appropriate location in the grid" do
       game = Gameplay.new
-      game.play_piece('X', 2)
-      expect(game.play_piece('O', 2)).to eq(
+      game.play_piece(2)
+      expect(game.play_piece(2)).to eq(
         [ [nil, nil, nil, nil, nil, nil, nil], 
           [nil, nil, nil, nil, nil, nil, nil],
           [nil, nil, nil, nil, nil, nil, nil],
@@ -31,12 +31,25 @@ describe Gameplay do
       queue = []
       3.times do
         queue.push(game.take_turns)
+        game.play_piece(rand(6))
       end
       expect(queue).to eq(['X', 'O', 'X'])
     end
   end
 
   describe "#win_conditions" do
+    it "continues the game if four across is not found" do
+      game = Gameplay.new
+      grid =  [ [nil, nil, nil, nil, nil, nil, nil], 
+                [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil],
+                ['X', 'X', nil, nil, nil, nil, nil],
+                ['X', 'X', 'X', 'O', 'O', 'O', nil] ]
+      expect(game.win_conditions(grid)).to be(false)
+    end
+
     it "ends the game if four across is found" do
       game = Gameplay.new
       grid =  [ [nil, nil, nil, nil, nil, nil, nil], 
@@ -44,9 +57,38 @@ describe Gameplay do
                 [nil, nil, nil, nil, nil, nil, nil],
                 [nil, nil, nil, nil, nil, nil, nil],
                 [nil, nil, nil, nil, nil, nil, nil],
-                [nil, nil, 'O', 'O', nil, nil, nil],
-                ['X', 'X', 'X', 'X', 'O', 'O', nil] ]
-      expect(game.win_conditions(grid)).to be(true)
+                ['X', nil, nil, nil, nil, nil, nil],
+                ['X', 'O', 'O', 'O', 'O', 'X', nil] ]
+      expect(game.win_conditions(grid, 'O')).to be(true)
+    end
+
+    it "ends the game if a vertical four in a row is found" do
+      game = Gameplay.new
+      grid =  [ [nil, nil, nil, nil, nil, nil, nil], 
+                [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, nil, nil],
+                [nil, 'O', nil, nil, nil, nil, nil],
+                [nil, 'O', nil, nil, nil, nil, nil],
+                ['X', 'O', nil, nil, nil, nil, nil],
+                ['X', 'O', 'X', 'O', 'X', 'X', nil] ]
+      expect(game.win_conditions(grid, 'O')).to be(true)
+    end
+
+    it "ends the game if a diagonal four in a row is found" do
+      game = Gameplay.new
+      grid =  [ [nil, nil, nil, nil, nil, nil, nil], 
+                [nil, nil, nil, nil, nil, nil, nil],
+                [nil, nil, nil, nil, nil, 'X', nil],
+                [nil, 'O', nil, nil, 'X', nil, nil],
+                [nil, 'X', 'O', 'X', nil, nil, nil],
+                ['X', 'O', 'X', 'X', nil, nil, nil],
+                ['O', 'O', 'X', 'O', 'X', 'X', nil] ]
+      expect(game.win_conditions(grid, 'X')).to be(true)
+    end
+
+    it "creates a random game" do
+      game = Gameplay.new
+      expect(game.random_generator).to be(true)
     end
   end
 end
